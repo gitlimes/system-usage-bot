fcbd8bbd84bddc7d53bb748babf173519357506b
gitlimes <git@limes.pink>
2023-07-30 13:29:04 +0200

    feat: make intro optional, edit defaults, format

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import 'dotenv/config';
+import "dotenv/config";
 import { createRestAPIClient } from "masto";
 import osu from "node-os-utils";
 
 const config = {
-    intro: "haii~ hewe awe teh system stats :3", // text that will prefix the stats in the toot
+    intro: "", // text that will prefix the stats in the toot
     interval: 0, // time in seconds between toots (set to 0 if manually triggering or using crontab)
     filledBarChar: "▰", // the character for the filled-in usage bar (can be an emoji)
     emptyBarChar: "▱", // the character for the empty part of the usage bar (can also be an emoji)
-    barLength: 20 // number of characters used for the usage bar
-}
+    barLength: 16, // number of characters used for the usage bar
+};
 
 const masto = createRestAPIClient({
     url: process.env.INSTANCE_URL,
@@ -16,7 +16,7 @@ const masto = createRestAPIClient({
 });
 
 function usageBar(perc) {
-    const filledN = Math.round(perc / 100 * config.barLength);
+    const filledN = Math.round((perc / 100) * config.barLength);
     const filled = config.filledBarChar.repeat(filledN);
     const empty = config.emptyBarChar.repeat(config.barLength - filledN);
     return filled + empty;
@@ -26,7 +26,7 @@ async function toot() {
     const cpu = await osu.cpu.usage();
     const drive = await osu.drive.used();
     const ram = await osu.mem.info();
-    const up = new Date(await osu.os.uptime() * 1000);
+    const up = new Date((await osu.os.uptime()) * 1000);
 
     const upDays = up.getUTCDate() - 1;
     const upHours = up.getUTCHours();
@@ -35,21 +35,31 @@ async function toot() {
 
     let humanReadableUp = "";
     if (upDays) {
-        let plural = (upDays !== 1) ? "s" : "";
+        let plural = upDays !== 1 ? "s" : "";
         humanReadableUp += `${upDays} day${plural}, `;
     }
-    if (upHours || (upDays > 0)) {
-        let plural = (upHours !== 1) ? "s" : "";
+    if (upHours || upDays > 0) {
+        let plural = upHours !== 1 ? "s" : "";
         humanReadableUp += `${upHours} hour${plural}, `;
     }
-    if (upMinutes || (upHours + upDays > 0)) {
-        let plural = (upMinutes !== 1) ? "s" : "";
+    if (upMinutes || upHours + upDays > 0) {
+        let plural = upMinutes !== 1 ? "s" : "";
         humanReadableUp += `${upMinutes} minute${plural} and `;
     }
-    let plural = (upSeconds !== 1) ? "s" : "";
+    let plural = upSeconds !== 1 ? "s" : "";
     humanReadableUp += `${upSeconds} second${plural}`;
 
-    const toot = `${config.intro}\n\nCPU:\n${usageBar(cpu)} (${cpu.toFixed(1)}%)\n\nRAM: ${Math.round(ram.usedMemMb)}MB/${Math.round(ram.totalMemMb)}MB\n${usageBar(ram.usedMemPercentage)} (${ram.usedMemPercentage.toFixed(1)}%)\n\nDisk (/): ${drive.usedGb}GB/${drive.totalGb}GB\n${usageBar(drive.usedPercentage)} (${parseFloat(drive.usedPercentage).toFixed(1)}%)\n\nUptime: ${humanReadableUp}`;
+    const toot = `${config.intro ? `${config.intro}\n\n` : ""}CPU:\n${usageBar(
+        cpu
+    )} (${cpu.toFixed(1)}%)\n\nRAM: ${Math.round(ram.usedMemMb)}MB/${Math.round(
+        ram.totalMemMb
+    )}MB\n${usageBar(ram.usedMemPercentage)} (${ram.usedMemPercentage.toFixed(
+        1
+    )}%)\n\nDisk (/): ${drive.usedGb}GB/${drive.totalGb}GB\n${usageBar(
+        drive.usedPercentage
+    )} (${parseFloat(drive.usedPercentage).toFixed(
+        1
+    )}%)\n\nUptime: ${humanReadableUp}`;
 
     const status = await masto.v1.statuses.create({
         status: toot,
